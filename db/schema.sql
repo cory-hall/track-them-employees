961ef190fc394df601cb7fe72561f2659737e2cf
Cory Hall <ComebackCory1@gmail.com>
2022-04-22 21:23:36 -0500

    added table creation

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS empRole;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE empRole (
+   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL(7,2),
+   department_id INTEGER,
+   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INTEGER,
+   CONSTRAINT fk_empRole FOREIGN KEY (role_id) REFERENCES empRole(id) ON DELETE SET NULL
+)
+
